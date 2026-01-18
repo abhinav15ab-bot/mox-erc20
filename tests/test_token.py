47fd44596993c5067b17200dd0cb5c2ac21ce91e
MutAnt <abhinavmalik300@gmail.com>
2026-01-18 23:16:55 +0530

    Update test_token.py

--- tests/test_token.py
+++ tests/test_token.py
@@ -6,3 +6,12 @@ def test_token_supply():
     snek_token = deploy()
     assert snek_token.totalSupply() == INITIAL_SUPPLY
 
+
+def test_token_emits_event():
+    snek_token = deploy()
+    with boa.env.prank(snek_token.owner()):
+        snek_token.transfer(RANDOM_USER, INITIAL_SUPPLY)
+        logs = snek_token.get_logs()
+        
+        log_owner = logs[0].sender
+        assert log_owner == snek_token.owner()
